808c6119270a4eea07d1fb9cdb4cb6e705b01b1b
Tiago <up201806754@fe.up.pt>
2022-11-15 11:47:21 +0000

    Implemented room and map_grid classes

--- src/game_lib/environment/room.py
+++ src/game_lib/environment/room.py
@@ -1,9 +1,24 @@
 """Class that stores state of a limited space inside the game environment."""
+from typing import Tuple
+from .map_grid import MapGrid
 
 
 class Room:
     """Class that stores state of a limited space inside the game environment."""
 
-    def __init__(self) -> None:
+    def __init__(
+        self,
+        room_id: str,
+        dimensions: Tuple[int, int],
+        map_image: str,
+        map_grids: Tuple[Tuple[MapGrid]],
+        soundtrack: str,
+    ) -> None:
         """Initialize Room instance."""
+        self.room_id = room_id
+        self.dimensions = dimensions
+        self.map_image = map_image  # convert to binary
+        self.map_grids = map_grids
+        self.soundtrack = soundtrack
+
         pass
