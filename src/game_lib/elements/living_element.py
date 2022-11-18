b09bf4861791a190269f0c6cb10a5ceb050a2703
cthadeufaria <crls.thadeu@gmail.com>
2022-11-18 18:48:07 +0000

    living_element is_dead() creation

--- src/game_lib/elements/living_element.py
+++ src/game_lib/elements/living_element.py
@@ -1,6 +1,7 @@
 """Any MovingElement that has health points."""
 from .moving_element import MovingElement
 from typing import Tuple
+import pygame
 
 
 class LivingElement(MovingElement):
@@ -24,10 +25,22 @@ class LivingElement(MovingElement):
         super().__init__(position, image_paths, dimensions, base_speed, velocity)
 
     def attack(self) -> None:
+        """Attacks enemies."""
         pass
 
-    def damaged(self) -> int:
-        pass
+    def is_dead(
+        self, rect: pygame.rect.Rect, attack_force: int, is_attack: bool
+    ) -> bool:
+        """Check if dead and decrease health points."""
+        if self.is_colliding(rect) and is_attack:
+            self.health_points += attack_force
+        if self.health_points <= 0:
+            self.health_points = 0
+            is_dead = True
+        else:
+            is_dead = False
+        return is_dead
 
-    def is_dead(self) -> bool:
+    def heal(self, hp: int) -> None:
+        """Heal living element increasing health points."""
         pass
