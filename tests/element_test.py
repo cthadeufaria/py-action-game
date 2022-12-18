75cc8ddebf19de0fed380446a2787817815b07fc
nhpf <nicholas@hopf.pt>
2022-12-18 22:53:11 +0000

    Adjust tests with new syntax for images in game Elements

--- tests/element_test.py
+++ tests/element_test.py
@@ -15,14 +15,14 @@ class CheckCollisionMethod(TestCase):
 
     def test_overlapping_elements(self):
         """Guarantees that overlapping elements do collide."""
-        e1 = Element(position=(100, 100), image_paths=["bat.png"], dimensions=(50, 30))
-        e2 = Element(position=(120, 120), image_paths=["ball.png"], dimensions=(30, 10))
+        e1 = Element(position=(100, 100), base_image_path="bat.png", dimensions=(50, 30))
+        e2 = Element(position=(120, 120), base_image_path="ball.png", dimensions=(30, 10))
         self.assertTrue(e1.is_colliding(e2))
         self.assertTrue(e2.is_colliding(e1))
 
     def test_separated_elements(self):
         """Guarantees that separated elements do not collide."""
-        e1 = Element(position=(200, 100), image_paths=["bat.png"], dimensions=(40, 20))
-        e2 = Element(position=(100, 50), image_paths=["ball.png"], dimensions=(40, 40))
+        e1 = Element(position=(200, 100), base_image_path="bat.png", dimensions=(40, 20))
+        e2 = Element(position=(100, 50), base_image_path="ball.png", dimensions=(40, 40))
         self.assertFalse(e1.is_colliding(e2))
         self.assertFalse(e2.is_colliding(e1))
--- tests/moving_element_test.py
+++ tests/moving_element_test.py
@@ -10,10 +10,10 @@ import pygame
 pygame.init()
 pygame.display.set_mode((2000, 2000), pygame.NOFRAME)
 
-top_wall = Element(position=(0, 0), image_paths=["ball.png"], dimensions=(100, 20))
-right_wall = Element(position=(80, 0), image_paths=["ball.png"], dimensions=(20, 100))
-bottom_wall = Element(position=(0, 80), image_paths=["ball.png"], dimensions=(100, 20))
-left_wall = Element(position=(0, 0), image_paths=["ball.png"], dimensions=(20, 100))
+top_wall = Element(position=(0, 0), base_image_path="ball.png", dimensions=(100, 20))
+right_wall = Element(position=(80, 0), base_image_path="ball.png", dimensions=(20, 100))
+bottom_wall = Element(position=(0, 80), base_image_path="ball.png", dimensions=(100, 20))
+left_wall = Element(position=(0, 0), base_image_path="ball.png", dimensions=(20, 100))
 
 collision_paths = {
     "top": {"velocity": (0, -1), "wall": top_wall},
@@ -32,7 +32,7 @@ class CheckMovingMethod(TestCase):
         for path in collision_paths:
             flying_element = MovingElement(
                 (45, 45),
-                image_paths=["bat.png"],
+                base_image_path="bat.png",
                 dimensions=(10, 10),
                 base_speed=40,
                 can_fly=True,
@@ -47,7 +47,7 @@ class CheckMovingMethod(TestCase):
         for path in collision_paths:
             flying_element = MovingElement(
                 (45, 45),
-                image_paths=["bat.png"],
+                base_image_path="bat.png",
                 dimensions=(10, 10),
                 base_speed=25,
                 can_fly=True,
@@ -62,7 +62,7 @@ class CheckMovingMethod(TestCase):
         for path in collision_paths:
             non_flying_element = MovingElement(
                 (45, 45),
-                image_paths=["bat.png"],
+                base_image_path="bat.png",
                 dimensions=(10, 10),
                 base_speed=40,
                 can_fly=False,
@@ -79,7 +79,7 @@ class CheckMovingMethod(TestCase):
         for path in collision_paths:
             non_flying_element = MovingElement(
                 (45, 45),
-                image_paths=["bat.png"],
+                base_image_path="bat.png",
                 dimensions=(10, 10),
                 base_speed=25,
                 can_fly=False,
